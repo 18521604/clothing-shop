323d53878ac1dc17fac8460b15a292124138456d
David Tuan <18521604@gm.uit.edu.vn>
2021-07-23 15:52:45 +0700

    added addItem and removeItem functionality in checkout-item component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,14 @@
 import { React } from "react";
 import { Link } from "react-router-dom";
-import { auth } from "../../firebase/firebase.utils";
 import { connect } from 'react-redux';
+import { createStructuredSelector } from "reselect";
+
+import { auth } from "../../firebase/firebase.utils";
 
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 // Special syntax when use SVG file
 import { ReactComponent as Logo } from "../../assets/4.4 crown.svg.svg";
@@ -34,9 +38,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,7 +1,6 @@
 export const addItemToCart = (cartItemToAdd, cartItems) => {
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
-    console.log(existingCartItem);
     if (existingCartItem) {
         return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id
@@ -11,4 +10,18 @@ export const addItemToCart = (cartItemToAdd, cartItems) => {
     }
 
     return [{ ...cartItemToAdd, quantity: 1 }, ...cartItems]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(item => item.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === cartItemToRemove.id ?
+            { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    )
 }
